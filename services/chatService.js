918a0b00d674b634d11ecd1f2a152f1ae118ca8c
tejpal singh <tej@dhwaniastro.com>
2026-07-23 16:50:10 +0530

    change for user

--- services/chatService.js
+++ services/chatService.js
@@ -602,7 +602,7 @@ export const finalizeChatSession = async (roomId, prisma, redis, astroId) => {
 };
 
 export const finalizeChatSessionByAdmin = async (roomId, prisma, redis, astroId) => {
-  console.log("finalizeChatSessionByAdmin---------:",roomId, prisma, redis, astroId);
+  console.log("finalizeChatSessionByAdmin---------:",roomId, astroId);
   let lockKey = null;
   let lockValue = null;
 

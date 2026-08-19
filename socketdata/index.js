3fd06f7423f22d79537e9a0f57daf523fff0cf43
tejpal singh <tej@dhwaniastro.com>
2026-08-19 13:18:44 +0530

    change for typing

--- socketdata/index.js
+++ socketdata/index.js
@@ -191,6 +191,7 @@ async function socketHandler(io, pubClient, subClient, redisClient) {
               break;
 
             case "astrologer_typing":
+              console.log("typing-----------:",data);
               io.to(data.roomid).emit("typing", data);
               break;
             case "queue_update":
